6ff10aed9e8cff4ffb7101067c3931598d9ad07e
varsheshPatel <patel5p4@uwindsor.ca>
2022-02-20 21:07:21 -0500

    Git firebase script added and Google auth api add

---
+++ Assets/Scripts/FirebaseManager.cs
@@ -0,0 +1,86 @@
+﻿using Firebase;
+using Firebase.Auth;
+using Firebase.Database;
+using UnityEngine;
+
+public class FirebaseManager : MonoBehaviour
+{
+    public static FirebaseManager Instance;
+    
+    private FirebaseApp app;
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+    private FirebaseDatabase db;
+
+
+    public FirebaseApp App
+    {
+        get => app;
+        set => app = value;
+    }
+
+    public FirebaseAuth Auth
+    {
+        get => auth;
+        set => auth = value;
+    }
+
+    public FirebaseDatabase Db
+    {
+        get => db;
+        set => db = value;
+    }
+
+    async void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+        var result = await FirebaseApp.CheckAndFixDependenciesAsync();
+        if (result == DependencyStatus.Available)
+        {
+            app = FirebaseApp.DefaultInstance;
+            auth = FirebaseAuth.DefaultInstance;
+            db = FirebaseDatabase.DefaultInstance;
+
+            auth.StateChanged += AuthStateChanged;
+            AuthStateChanged(this, null);
+        }
+        else
+        {
+            Debug.Log("Error");
+        }
+    }
+    
+    void AuthStateChanged(object sender, System.EventArgs eventArgs) {
+        if (auth.CurrentUser != user) {
+            bool signedIn = user != auth.CurrentUser && auth.CurrentUser != null;
+            if (!signedIn && user != null) {
+                Debug.Log("Signed out " + user.UserId);
+            }
+            
+            user = auth.CurrentUser;
+            
+            if (signedIn) {
+                Debug.Log("Signed in " + user.UserId);
+            }
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GoogleSignInHelper.cs
@@ -0,0 +1,142 @@
+﻿// <copyright file="SigninSampleScript.cs" company="Google Inc.">
+// Copyright (C) 2017 Google Inc. All Rights Reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations
+
+using TMPro;
+
+namespace SignInSample {
+  using System;
+  using System.Collections.Generic;
+  using System.Threading.Tasks;
+  using Google;
+  using UnityEngine;
+  using UnityEngine.UI;
+
+  public class GoogleSignInHelper : MonoBehaviour
+  {
+    public TextMeshProUGUI userName, loginText;
+    public Button loginButton;
+
+    public string webClientId = "<your client id here>";
+
+    private GoogleSignInConfiguration configuration;
+
+    public static string AccessToken = "";
+
+    // Defer the configuration creation until Awake so the web Client ID
+    // Can be set via the property inspector in the Editor.
+    void Awake() {
+      configuration = new GoogleSignInConfiguration {
+            WebClientId = webClientId,
+            RequestIdToken = true
+      };
+      
+      loginButton.onClick.AddListener(OnSignIn);
+    }
+
+    public void OnSignIn() {
+      GoogleSignIn.Configuration = configuration;
+      GoogleSignIn.Configuration.UseGameSignIn = false;
+      GoogleSignIn.Configuration.RequestIdToken = true;
+      AddStatusText("Calling SignIn");
+
+      GoogleSignIn.DefaultInstance.SignIn().ContinueWith(
+        OnAuthenticationFinished);
+    }
+
+    public void OnSignOut() {
+      AddStatusText("Calling SignOut");
+      GoogleSignIn.DefaultInstance.SignOut();
+      userName.text = "";
+      loginText.text = "Login with Google";
+      
+      loginButton.onClick.RemoveAllListeners();
+      loginButton.onClick.AddListener(OnSignIn);
+    }
+
+    public void OnDisconnect() {
+      AddStatusText("Calling Disconnect");
+      GoogleSignIn.DefaultInstance.Disconnect();
+    }
+
+    internal void OnAuthenticationFinished(Task<GoogleSignInUser> task) {
+      if (task.IsFaulted) {
+        using (IEnumerator<System.Exception> enumerator =
+                task.Exception.InnerExceptions.GetEnumerator()) {
+          if (enumerator.MoveNext()) {
+            GoogleSignIn.SignInException error =
+                    (GoogleSignIn.SignInException)enumerator.Current;
+            AddStatusText("Got Error: " + error.Status + " " + error.Message);
+          } else {
+            AddStatusText("Got Unexpected Exception?!?" + task.Exception);
+          }
+        }
+      } 
+      else if(task.IsCanceled) {
+        AddStatusText("Canceled");
+      } 
+      else
+      {
+        AccessToken = task.Result.IdToken;
+        userName.text = "Welcome: " + task.Result.DisplayName + " !";
+        loginText.text = "Logout";
+        
+        loginButton.onClick.RemoveAllListeners();
+        loginButton.onClick.AddListener(OnSignOut);
+        AddStatusText("Welcome: " + task.Result.DisplayName + "!");
+      }
+    }
+
+    public void ChangeScene()
+    {
+      
+    }
+
+    public void OnSignInSilently() {
+      GoogleSignIn.Configuration = configuration;
+      GoogleSignIn.Configuration.UseGameSignIn = false;
+      GoogleSignIn.Configuration.RequestIdToken = true;
+      AddStatusText("Calling SignIn Silently");
+
+      GoogleSignIn.DefaultInstance.SignInSilently()
+            .ContinueWith(OnAuthenticationFinished);
+    }
+
+
+    public void OnGamesSignIn() {
+      GoogleSignIn.Configuration = configuration;
+      GoogleSignIn.Configuration.UseGameSignIn = true;
+      GoogleSignIn.Configuration.RequestIdToken = false;
+
+      AddStatusText("Calling Games SignIn");
+
+      GoogleSignIn.DefaultInstance.SignIn().ContinueWith(
+        OnAuthenticationFinished);
+    }
+
+    private List<string> messages = new List<string>();
+    void AddStatusText(string text) {
+      if (messages.Count == 5) {
+        messages.RemoveAt(0);
+      }
+      messages.Add(text);
+      string txt = "";
+      foreach (string s in messages) {
+        txt += "\n" + s;
+      }
+      Debug.Log(txt);
+      //statusText.text = txt;
+    }
+  }
+}
